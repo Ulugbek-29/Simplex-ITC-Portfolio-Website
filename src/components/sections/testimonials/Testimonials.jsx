15dd09bdbd670bd1d34d0c6f9a0c90e7b9ccd6c6
ulugbek-29-coder <ulugbek1993uzb@gmail.com>
2024-01-13 19:34:26 +0500

    avatar image url corrected

--- src/components/sections/testimonials/Testimonials.jsx
+++ src/components/sections/testimonials/Testimonials.jsx
@@ -35,7 +35,7 @@ const Testimonials = () => {
         </SwiperSlide>
         <SwiperSlide>
           <TestimonialCard
-            imageUrl="images/Ellipse 1.png"
+            imageUrl="images/testimonials-section-images/Ellipse 1.png"
             description="Now, i can track my business activity with easier and have a great understandable to operate the products"
             rating="4.3"
             name="Janne Cooper"
