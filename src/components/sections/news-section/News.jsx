320570258455e8672e7bd30c611d249d8d15add5
ulugbek-29-coder <ulugbek1993uzb@gmail.com>
2024-01-13 17:21:19 +0500

    news section is complete

--- src/components/sections/news-section/News.jsx
+++ src/components/sections/news-section/News.jsx
@@ -12,58 +12,202 @@ const News = () => {
         </div>
         <div className={styles.container}>
           <div className={styles.box}>
-            <div className={styles.image}>image</div>
+            <div className={styles.image}>
+              <img
+                className={styles.singleImage}
+                src="images/news-section-images/image 16.png"
+                alt=""
+              />
+            </div>
             <div className={styles.content}>
-              <span>UI Design | 22 May 2023</span>
-              <h2>What is UI Design in Front End Design?</h2>
-              <p>
+              <span className={styles.cardSubtitle}>
+                UI Design | 22 May 2023
+              </span>
+              <h2 className={styles.cardTitle}>
+                What is UI Design in Front End Design?
+              </h2>
+              <p className={styles.cardContent}>
                 But I must explain to you how all this mistaken idea of
                 denouncing pleasure and praising pain was born and I will give
                 you a complete account of the system
               </p>
-              <a href="/">Read more</a>
+              <a className={styles.link} href="/">
+                Read more{" "}
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                  >
+                    <path
+                      d="M11.8935 3.40821L3.4082 11.8935"
+                      stroke="white"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M5.06272 3.42261C5.06272 3.42261 10.3298 1.84492 11.8922 3.40734C13.4558 4.97089 11.8787 10.2386 11.8787 10.2386"
+                      stroke="white"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </span>
+              </a>
             </div>
           </div>
           <div className={styles.box}>
-            <div className={styles.image}>image</div>
+            <div className={styles.image}>
+              <img
+                className={styles.singleImage}
+                src="images/news-section-images/image 17.png"
+                alt=""
+              />
+            </div>
             <div className={styles.content}>
               {" "}
-              <span>UI Design | 22 May 2023</span>
-              <h2>What is UI Design in Front End Design?</h2>
-              <p>
+              <span className={styles.cardSubtitle}>
+                UI Design | 22 May 2023
+              </span>
+              <h2 className={styles.cardTitle}>
+                What is UI Design in Front End Design?
+              </h2>
+              <p className={styles.cardContent}>
                 But I must explain to you how all this mistaken idea of
                 denouncing pleasure and praising pain was born and I will give
                 you a complete account of the system
               </p>
-              <a href="/">Read more</a>
+              <a className={styles.link} href="/">
+                Read more{" "}
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                  >
+                    <path
+                      d="M11.8935 3.40821L3.4082 11.8935"
+                      stroke="white"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M5.06272 3.42261C5.06272 3.42261 10.3298 1.84492 11.8922 3.40734C13.4558 4.97089 11.8787 10.2386 11.8787 10.2386"
+                      stroke="white"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </span>
+              </a>
             </div>
           </div>
           <div className={styles.box}>
-            <div className={styles.image}>image</div>
+            <div className={styles.image}>
+              <img
+                className={styles.singleImage}
+                src="images/news-section-images/image 16.png"
+                alt=""
+              />
+            </div>
             <div className={styles.content}>
               {" "}
-              <span>UI Design | 22 May 2023</span>
-              <h2>What is UI Design in Front End Design?</h2>
-              <p>
+              <span className={styles.cardSubtitle}>
+                UI Design | 22 May 2023
+              </span>
+              <h2 className={styles.cardTitle}>
+                What is UI Design in Front End Design?
+              </h2>
+              <p className={styles.cardContent}>
                 But I must explain to you how all this mistaken idea of
                 denouncing pleasure and praising pain was born and I will give
                 you a complete account of the system
               </p>
-              <a href="/">Read more</a>
+              <a className={styles.link} href="/">
+                Read more{" "}
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                  >
+                    <path
+                      d="M11.8935 3.40821L3.4082 11.8935"
+                      stroke="white"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M5.06272 3.42261C5.06272 3.42261 10.3298 1.84492 11.8922 3.40734C13.4558 4.97089 11.8787 10.2386 11.8787 10.2386"
+                      stroke="white"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </span>
+              </a>
             </div>
           </div>
           <div className={styles.box}>
-            <div className={styles.image}>image</div>
+            <div className={styles.image}>
+              <img
+                className={styles.singleImage}
+                src="images/news-section-images/image 17.png"
+                alt=""
+              />
+            </div>
             <div className={styles.content}>
               {" "}
-              <span>UI Design | 22 May 2023</span>
-              <h2>What is UI Design in Front End Design?</h2>
-              <p>
+              <span className={styles.cardSubtitle}>
+                UI Design | 22 May 2023
+              </span>
+              <h2 className={styles.cardTitle}>
+                What is UI Design in Front End Design?
+              </h2>
+              <p className={styles.cardContent}>
                 But I must explain to you how all this mistaken idea of
                 denouncing pleasure and praising pain was born and I will give
                 you a complete account of the system
               </p>
-              <a href="/">Read more</a>
+              <a className={styles.link} href="/">
+                Read more{" "}
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                  >
+                    <path
+                      d="M11.8935 3.40821L3.4082 11.8935"
+                      stroke="white"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M5.06272 3.42261C5.06272 3.42261 10.3298 1.84492 11.8922 3.40734C13.4558 4.97089 11.8787 10.2386 11.8787 10.2386"
+                      stroke="white"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </span>
+              </a>
             </div>
           </div>
         </div>
